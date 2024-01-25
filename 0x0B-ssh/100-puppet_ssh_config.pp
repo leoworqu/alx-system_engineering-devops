f566635d14d62329d68b53ddeda3c4a2fbf6792e
leoworqu <leoworqu@gmail.com>
2024-01-24 17:55:20 -0800

    puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,7 @@
+#changes SSH config file
+
+exec { 'echo':
+  path => 'usr/bin:/bin',
+  command => 'echo " IdentifyFile ~/.ssh/school\n PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns => [0, 1],
+}
