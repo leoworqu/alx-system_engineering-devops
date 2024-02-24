f2590d0431854b535f1505749e5b8c1779f80c93
leoworqu <leoworqu@gmail.com>
2024-02-23 22:12:35 -0800

    installhj man

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 # Puppet manifest to install Flask using pip3 in a virtual environment
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+package { 'python3-pip':
+  ensure => installed,
 }
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+}ackage { 'flask':
