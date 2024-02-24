9e7fb05cf8358e21158e33a5c5105a15166b2ca7
leoworqu <leoworqu@gmail.com>
2024-02-23 23:01:31 -0800

    comman

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 #!/usr/bin/pup
 # Install Flask version 2.1.0
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin'],
-  require => Package['python3-pip'],
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
