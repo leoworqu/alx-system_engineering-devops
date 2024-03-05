05f31914f65bfafb95ec69a19e9b29fc54478d3f
leoworqu <leoworqu@gmail.com>
2024-03-05 01:58:00 -0800

    api adv.

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    Get the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers of the subreddit. Returns 0 if the subreddit is invalid.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,6 @@
+this is the read me
+0x16. API advanced
+Python
+Scripting
+Back-end
+API
