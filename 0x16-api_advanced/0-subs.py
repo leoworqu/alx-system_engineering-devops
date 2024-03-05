e3f21949bfef3d05a945d51b83b689a64a4fd5cc
leoworqu <leoworqu@gmail.com>
2024-03-05 02:04:13 -0800

    api adv.g

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-
+#!/usr/bin/python3
+"""
+Function that queries the Reddit API and returns
+the number of subscribers for a given subreddit.
+"""
 import requests
+
 
 def number_of_subscribers(subreddit):
     """
     Get the number of subscribers for a given subreddit.
-
     Args:
         subreddit (str): The name of the subreddit.
-
     Returns:
         int: The number of subscribers of the subreddit. Returns 0 if the subreddit is invalid.
     """
