86d33d41cfab3a33040dffb2d6785950b30c53a1
leoworqu <132668626+leoworqu@users.noreply.github.com>
2024-03-06 12:32:15 +0300

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# changes all the phpp extensions in wp_setting file with php
+# changes all the .phpp extensions in wp_setting file with .php
 
 exec { 'replace_phpp_with_php':
   command     => 'sed -i "s/\.phpp/.php/g" /var/www/html/wp-settings.php',
