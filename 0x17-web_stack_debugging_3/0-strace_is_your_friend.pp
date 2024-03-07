c6c70a59606d05d288f8377de0631881704afe82
leoworqu <132668626+leoworqu@users.noreply.github.com>
2024-03-07 06:50:02 +0300

    your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # changes all the .phpp extensions in wp_setting file with .php
 
 exec { 'replace_phpp_with_php':
-  command     => 'sed -i "s/\.phpp/.php/g" /var/www/html/wp-settings.php',
-  path        => '/bin:/usr/bin',
+  command => 'sed -i "s/\.phpp/.php/g" /var/www/html/wp-settings.php',
+  path    => '/bin:/usr/bin',
 }
