d340657965a0e10fc365607db7e1372e66515e46
leoworqu <132668626+leoworqu@users.noreply.github.com>
2024-03-14 08:18:23 +0300

    Update 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,6 @@
 # Fix problem of "Too many open files"
 
-exec {'user_limit':
+exec { 'update_limits':
   provider => shell,
-  command  => 'sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
-  before   => Exec['limits'],
-}
-
-exec {'limits':
-  provider => shell,
-  command  => 'sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
+  command  => 'sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf; sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
 }
