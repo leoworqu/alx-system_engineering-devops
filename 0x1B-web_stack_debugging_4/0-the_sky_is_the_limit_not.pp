27100ceb982edd515d5c51a2006934ed3d5a1ce3
leoworqu <132668626+leoworqu@users.noreply.github.com>
2024-03-15 06:55:15 +0300

    pppppppppppp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # replace rhe ulimit
 
 exec { 'increase file limit for nginx':
-  onlyif  => 'test -e /etc/default/nginx',
-  command => "/bin/bash -c 'sed -i \"5s/[0-9]\\+/$$(ulimit -n)/\" /etc/default/nginx && service nginx restart'",
+  onlyif   => 'test -e /etc/default/nginx',
+  command  => "/bin/bash -c 'sed -i \"5s/[0-9]\\+/$$(ulimit -n)/\" /etc/default/nginx && service nginx restart'",
   provider => shell,
 }
